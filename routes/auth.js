18e0fc265376458466e76f96eb2c6d28da699cf9
sailendrachettri <sailendra9083@gmail.com>
2024-05-01 22:54:04 +0530

    handle logout and cookie removed

--- routes/auth.js
+++ routes/auth.js
@@ -76,7 +76,7 @@ router.get('/profile', (req, res) => {
 // ROUTE 4: Handle logout
 router.post('/logout', (req, res) => {
     try {
-        res.cookie('auth_token', '').json('ok'); // removing auth_token from cookie
+        res.cookie('jwt_token', '').json('ok'); // removing auth_token from cookie
     } catch (err) {
         res.status(500).json(err);
     }
