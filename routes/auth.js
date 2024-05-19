387447e29d2707eba809ce3e6135a6919463b73a
sailendrachettri <sailendra9083@gmail.com>
2024-05-19 14:52:06 +0530

    same site cookie none

--- routes/auth.js
+++ routes/auth.js
@@ -51,7 +51,7 @@ router.post('/login', async (req, res) => {
     // set details in cookies
     success = true;
     res.cookie('jwt_token', jwt_token, {
-        sameSite: 'None',
+        SameSite: 'None',
         secure: true
     }).status(200).json({ success, message: "Logged In scuessful!", jwt_token });
 })
